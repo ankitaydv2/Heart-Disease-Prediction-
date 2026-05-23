f5a663b609949894d6ed8d89894acb2484aca082
Ankita Yadav <ankitaydv1825@gmail.com>
2026-05-23 16:54:03 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.title("Heart Disease Prediction")
+st.write("This is a Machine Learning project using Logistic Regression and Decision Tree.")
+
+st.subheader("Model Accuracy")
+st.write("Logistic Regression: 88.52%")
+st.write("Decision Tree: 85.24%")
